3829ce28514973e3a0f773d01ae93834cb2cf57c
pwgawzynski <pgawzynski.backup@gmail.com>
2022-08-25 18:03:39 +0200

    Added meta for mobile devices

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { LoadAnimation } from './AtomicComponents/Pages/LoadAnimation';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
