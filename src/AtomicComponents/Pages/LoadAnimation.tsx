ba69f7b486d3ca892321b2018e1987ea57d3b9ed
pwgawzynski <pgawzynski.backup@gmail.com>
2022-08-25 18:06:24 +0200

    Added loading animation page

--- src/AtomicComponents/Pages/LoadAnimation.tsx
+++ src/AtomicComponents/Pages/LoadAnimation.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { StyledDivContainer } from '../Atoms/StyledDivContainer';
+import { AnimatedLoadingLogo } from '../Molecules/AnimatedLoadingLogo';
 
 export const LoadAnimation = () => {
-  return <div />;
+  return (
+    <StyledDivContainer width="100vw" height="100vh">
+      <AnimatedLoadingLogo />
+    </StyledDivContainer>
+  );
 };
