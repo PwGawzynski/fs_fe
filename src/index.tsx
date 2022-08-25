3d8b10416ba5e99d2abda0b50c8c21d47dc1fab4
pwgawzynski <pgawzynski.backup@gmail.com>
2022-08-25 12:58:56 +0200

    Prepared Enviroment For run with eslint airrbnb and husky

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
+import { LoadAnimation } from './AtomicComponents/Pages/LoadAnimation';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <>
-
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LoadAnimation />} />
+    </Routes>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
