0ff632ed85cb419b090f08f6b0566ecaf9de7e20
pwgawzynski <pgawzynski.backup@gmail.com>
2022-08-29 10:07:20 +0200

    Added Input styled template to create Input component witch is reusable

---
+++ src/AtomicComponents/Atoms/Input.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+  width: 100%;
+  box-sizing: border-box;
+  margin-bottom: 4%;
+  border: 1px solid black;
+  border-radius: 5px;
+  height: 2em;
+`;
