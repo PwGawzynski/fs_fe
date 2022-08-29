f9bbc394829cb6dab183b16192a8fefcd0ac916e
pwgawzynski <pgawzynski.backup@gmail.com>
2022-08-29 09:59:28 +0200

    Added Logo image to be shown on register page

--- src/AtomicComponents/Molecules/AnimatedLoadingLogo.tsx
+++ src/AtomicComponents/Molecules/AnimatedLoadingLogo.tsx
@@ -1,12 +1,12 @@
 import { StyledDivContainer } from '../Atoms/StyledDivContainer';
 import { Img } from '../Atoms/Imge';
-import LogoImg from '../../assets/images/logo.png';
 import LoadingGip from '../../assets/images/spinner.gif';
+import { LogoImg } from '../Atoms/LogoImg';
 
 export const AnimatedLoadingLogo = () => {
   return (
     <StyledDivContainer width="100%" display="flex" flexDirection="column">
-      <Img width="80%" src={LogoImg} margin="0 0 30% 0" />
+      <LogoImg width="80%" margin="0 0 30% 0" />
       <Img src={LoadingGip} width="10%" />
     </StyledDivContainer>
   );
