a51599e19558516e84467fef6e5887db521b3243
pwgawzynski <pgawzynski.backup@gmail.com>
2022-08-29 10:05:34 +0200

    Added FormTemplate.tsx molecule which is container for form

---
+++ src/AtomicComponents/Atoms/FormTemplate.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const FormTemplate = styled.form`
+  width: 60%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
