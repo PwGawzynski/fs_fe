a3a2034d486ed9ac34c01a3bdfd66033006a24fb
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-07 07:37:53 +0200

    added TopBar to Desktop page

--- src/AtomicComponents/Pages/Desktop.tsx
+++ src/AtomicComponents/Pages/Desktop.tsx
@@ -1,16 +1,10 @@
-import { useEffect, useState } from 'react';
 import { StyledDivContainer } from '../Atoms/StyledDivContainer';
-import { Api } from '../../Utils/Api/Api';
+import { TopBar } from '../Organism/TopBar';
 
 export const Desktop = () => {
-  const [test, setTest] = useState('');
-  useEffect(() => {
-    (async () => {
-      const data = await Api.sendTest();
-      if (data && data.message) {
-        setTest(data.message);
-      }
-    })();
-  }, []);
-  return <StyledDivContainer>{test}</StyledDivContainer>;
+  return (
+    <StyledDivContainer>
+      <TopBar />
+    </StyledDivContainer>
+  );
 };
