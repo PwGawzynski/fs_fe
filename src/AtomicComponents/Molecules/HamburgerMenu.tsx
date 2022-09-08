373b6e434fec76e7947343d5c5c57d0b23cbc1bb
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-08 19:54:34 +0200

    prepare for merge to develop for creating new brunch

---
+++ src/AtomicComponents/Molecules/HamburgerMenu.tsx
@@ -0,0 +1,9 @@
+interface Props {
+  // eslint-disable-next-line react/no-unused-prop-types
+  isOn: boolean;
+}
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const HamburgerMenu = (props: Props) => {
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return <></>;
+};
