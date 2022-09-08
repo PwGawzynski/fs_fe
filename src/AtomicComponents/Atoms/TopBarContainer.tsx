4b7a7f24a08a151b4fcde7f3f709a48128a55d7f
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-08 19:15:32 +0200

    Added top bar atom

---
+++ src/AtomicComponents/Atoms/TopBarContainer.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { StyledDivContainer } from './StyledDivContainer';
+
+export const TopBarContainer = styled(StyledDivContainer)`
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  box-shadow: 2px 2px 10px #333333;
+  padding-left: 5vw;
+  flex-direction: row;
+  justify-content: flex-start;
+  min-height: 8vh;
+`;
