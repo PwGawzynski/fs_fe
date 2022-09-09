1d6e7821f4be15d29d8bebd95582c5d67cf5eaae
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-09 16:30:04 +0200

    added styles for top bar to be fixed to top side of page

--- src/AtomicComponents/Atoms/TopBarContainer.tsx
+++ src/AtomicComponents/Atoms/TopBarContainer.tsx
@@ -1,11 +1,22 @@
 import styled from 'styled-components';
 import { StyledDivContainer } from './StyledDivContainer';
 
+interface Props {
+  menuOn: boolean;
+}
+
 export const TopBarContainer = styled(StyledDivContainer)`
   border-bottom-left-radius: 10px;
   border-bottom-right-radius: 10px;
   box-shadow: 2px 2px 10px #333333;
   flex-direction: row;
+  align-items: flex-start;
+  box-sizing: border-box;
   justify-content: flex-start;
-  min-height: 8vh;
+  min-height: ${(props: Props) => (props.menuOn ? '90vh' : '8vh')};
+  transition-duration: 2000ms;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
 `;
