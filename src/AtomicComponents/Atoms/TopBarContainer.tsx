7aea3f5cc8e72bb422ae38b372fc433c9aa4eabb
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-09 16:46:35 +0200

    hanged styles

--- src/AtomicComponents/Atoms/TopBarContainer.tsx
+++ src/AtomicComponents/Atoms/TopBarContainer.tsx
@@ -9,14 +9,15 @@ export const TopBarContainer = styled(StyledDivContainer)`
   border-bottom-left-radius: 10px;
   border-bottom-right-radius: 10px;
   box-shadow: 2px 2px 10px #333333;
-  flex-direction: row;
+  flex-direction: column;
   align-items: flex-start;
   box-sizing: border-box;
   justify-content: flex-start;
-  min-height: ${(props: Props) => (props.menuOn ? '90vh' : '8vh')};
+  height: ${(props: Props) => (props.menuOn ? '90vh' : '8vh')};
   transition-duration: 2000ms;
   position: fixed;
   top: 0;
   left: 0;
   z-index: 100;
+  overflow: hidden;
 `;
