190de2c6bb045958f3a52c3054eac0f1664c8afc
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-09 11:13:59 +0200

    rem padding

--- src/AtomicComponents/Atoms/TopBarContainer.tsx
+++ src/AtomicComponents/Atoms/TopBarContainer.tsx
@@ -5,7 +5,6 @@ export const TopBarContainer = styled(StyledDivContainer)`
   border-bottom-left-radius: 10px;
   border-bottom-right-radius: 10px;
   box-shadow: 2px 2px 10px #333333;
-  padding-left: 5vw;
   flex-direction: row;
   justify-content: flex-start;
   min-height: 8vh;
