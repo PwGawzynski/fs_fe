c894dc4e66027b930729f56387f7f4d51401cf40
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-09 11:14:40 +0200

    added on click handler, done on click action for animation

--- src/AtomicComponents/Organism/TopBar.tsx
+++ src/AtomicComponents/Organism/TopBar.tsx
@@ -1,13 +1,20 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Api } from '../../Utils/Api/Api';
 import { ProfilePicture } from '../Molecules/ProfilePicture';
 import { TopBarContainer } from '../Atoms/TopBarContainer';
 import { HamburgerMenu } from '../Molecules/HamburgerMenu';
+import { StyledDivContainer } from '../Atoms/StyledDivContainer';
+
+const handleClickOnHamburger = (
+  setMenu: React.Dispatch<React.SetStateAction<'block' | 'none'>>,
+) => {
+  setMenu((prevState) => (prevState === 'block' ? 'none' : 'block'));
+};
 
 export const TopBar = () => {
   const [profile, setProfile] = useState('');
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [menuOn, setMenuOn] = useState('block' as 'block' | 'none');
+
   useEffect(() => {
     (async () => {
       const file = await Api.getUserProfilePicture();
@@ -15,10 +22,20 @@ export const TopBar = () => {
       setProfile(img);
     })();
   }, []);
+
   return (
-    <TopBarContainer>
-      <ProfilePicture link={profile} />
-      <HamburgerMenu display={menuOn} />
+    <TopBarContainer onClick={() => handleClickOnHamburger(setMenuOn)}>
+      <StyledDivContainer width="20%">
+        <ProfilePicture link={profile} />
+      </StyledDivContainer>
+
+      <StyledDivContainer
+        width="80%"
+        justifyContent="flex-end"
+        alignItems="center"
+      >
+        <HamburgerMenu display={menuOn} />
+      </StyledDivContainer>
     </TopBarContainer>
   );
 };
