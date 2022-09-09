9c9c6b1720338b15926b6a5c7d45a2538c55d89d
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-09 11:13:45 +0200

    added new molecule for contains slices in Hamburger menu

---
+++ src/AtomicComponents/Atoms/SlicesContainer.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { StyledDivContainer } from './StyledDivContainer';
+
+export const SlicesContainer = styled(StyledDivContainer)`
+  flex-direction: column;
+  margin: 0 10% 0 0;
+  justify-content: center;
+  width: 10%;
+  height: 5vh;
+  box-sizing: border-box;
+  align-items: center;
+`;
