b11c34a2f3ee56eeb7a49a517993e60f3250d9df
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-09 11:13:01 +0200

    added on click animation

--- src/AtomicComponents/Atoms/HamburgerSlice.tsx
+++ src/AtomicComponents/Atoms/HamburgerSlice.tsx
@@ -6,9 +6,20 @@ interface Props {
 
 export const HamburgerSlice = styled.div`
   width: 100%;
-  min-height: 0.45vh;
-  margin-bottom: 20%;
+  min-height: 0.48vh;
   background-color: #013237;
   border-radius: 10px;
-  display: ${(props: Props) => props.display};
+  display: block;
+  transition-duration: 2000ms;
+  transition-timing-function: ease-out;
+  transform: ${(props: Props) =>
+    props.display === 'none' ? 'translateY(50%) rotate(45deg)' : 'none'};
+  &:nth-child(2) {
+    margin: 15% 0;
+    display: ${(props: Props) => props.display};
+  }
+  &:nth-child(3) {
+    transform: ${(props: Props) =>
+      props.display === 'none' ? 'translateY(-50%) rotate(-45deg)' : 'none'};
+  }
 `;
