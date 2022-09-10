82efe7327ee8b98bb13ea2357d82782cece7e49a
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-10 08:48:37 +0200

    Refactored

--- src/AtomicComponents/Organism/TopBar.tsx
+++ src/AtomicComponents/Organism/TopBar.tsx
@@ -1,43 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { Api } from '../../Utils/Api/Api';
-import { ProfilePicture } from '../Molecules/ProfilePicture';
+import React, { useState } from 'react';
 import { TopBarContainer } from '../Atoms/TopBarContainer';
-import { HamburgerMenu } from '../Molecules/HamburgerMenu';
-import { InsideTopBarContainer } from '../Atoms/InsideTopBarContainer';
-
-const handleClickOnHamburger = (
-  setMenu: React.Dispatch<React.SetStateAction<'block' | 'none'>>,
-) => {
-  setMenu((prevState) => (prevState === 'block' ? 'none' : 'block'));
-};
+import { RolledTopBarContent } from '../Atoms/RolledTopBarContent';
+import { ProfilePhoto } from './ProfilePhoto';
+import { Hamburger } from './Hamburger';
 
 export const TopBar = () => {
-  const [profile, setProfile] = useState('');
   const [menuOn, setMenuOn] = useState('block' as 'block' | 'none');
 
-  useEffect(() => {
-    (async () => {
-      const file = await Api.getUserProfilePicture();
-      const img = URL.createObjectURL(file);
-      setProfile(img);
-    })();
-  }, []);
-
   return (
-    <TopBarContainer
-      menuOn={menuOn === 'none'}
-      onClick={() => handleClickOnHamburger(setMenuOn)}
-      height="auto"
-    >
-      <InsideTopBarContainer width="100%">
-        <InsideTopBarContainer width="20%">
-          <ProfilePicture link={profile} />
-        </InsideTopBarContainer>
-
-        <InsideTopBarContainer width="80%" justifyContent="flex-end">
-          <HamburgerMenu display={menuOn} />
-        </InsideTopBarContainer>
-      </InsideTopBarContainer>
+    <TopBarContainer menuOn={menuOn === 'none'} height="auto">
+      <RolledTopBarContent width="100%">
+        <ProfilePhoto />
+        <Hamburger menuOn={menuOn} setMenuOn={setMenuOn} />
+      </RolledTopBarContent>
     </TopBarContainer>
   );
 };
