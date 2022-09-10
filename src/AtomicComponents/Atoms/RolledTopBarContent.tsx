5734f275b401be976ac9a1d7a50c7505dcb7ddbe
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-10 08:48:16 +0200

    Changed name

--- src/AtomicComponents/Atoms/RolledTopBarContent.tsx
+++ src/AtomicComponents/Atoms/RolledTopBarContent.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { StyledDivContainer } from './StyledDivContainer';
 
-export const InsideTopBarContainer = styled(StyledDivContainer)`
+export const RolledTopBarContent = styled(StyledDivContainer)`
   margin: 1vh 0 0 0;
   height: 6vh;
   box-sizing: border-box;
