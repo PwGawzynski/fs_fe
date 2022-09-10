1cc3d8e4034f318461868f5a25ebe4ae34d4b39e
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-10 08:46:50 +0200

    Moved onClick even on only hamburger menu, not all label

--- src/AtomicComponents/Molecules/HamburgerMenu.tsx
+++ src/AtomicComponents/Molecules/HamburgerMenu.tsx
@@ -1,13 +1,15 @@
+import React from 'react';
 import { HamburgerSlice } from '../Atoms/HamburgerSlice';
 import { SlicesContainer } from '../Atoms/SlicesContainer';
 
 interface Props {
   display: 'block' | 'none';
+  onClick: () => void;
 }
 export const HamburgerMenu = (props: Props) => {
-  const { display } = props;
+  const { display, onClick } = props;
   return (
-    <SlicesContainer>
+    <SlicesContainer onClick={onClick}>
       <HamburgerSlice display={display} />
       <HamburgerSlice display={display} />
       <HamburgerSlice display={display} />
