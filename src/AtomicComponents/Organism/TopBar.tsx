fc7beb7ea421dae7e05fc745258922c209f7ed3c
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-11 13:19:46 +0200

    Refactoring split to components

--- src/AtomicComponents/Organism/TopBar.tsx
+++ src/AtomicComponents/Organism/TopBar.tsx
@@ -1,22 +1,15 @@
 import React, { useState } from 'react';
-import { TopBarContainer, TopBarHidden } from '../Atoms/TopBarContainer';
-import { RolledTopBarContent } from '../Atoms/RolledTopBarContent';
-import { ProfilePhoto } from './ProfilePhoto';
-import { Hamburger } from './Hamburger';
-import { LogOutButton } from './LogOutButton';
+import { TopBarContainer } from '../Atoms/TopBarContainer';
+import { TopBarStillVisible } from './TopBarStillVisible';
+import { TopBarWhenOpeContext } from './TopBarWhenOpeContext';
 
 export const TopBar = () => {
   const [menuOn, setMenuOn] = useState('block' as 'block' | 'none');
 
   return (
     <TopBarContainer menuOn={menuOn === 'none'} height="auto">
-      <RolledTopBarContent width="100%">
-        <ProfilePhoto />
-        <Hamburger menuOn={menuOn} setMenuOn={setMenuOn} />
-      </RolledTopBarContent>
-      <TopBarHidden display={menuOn}>
-        <LogOutButton />
-      </TopBarHidden>
+      <TopBarStillVisible menuOn={menuOn} setMenuOn={setMenuOn} />
+      <TopBarWhenOpeContext menuOn={menuOn} />
     </TopBarContainer>
   );
 };
