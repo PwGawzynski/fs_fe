e9b09a1f2f35179fdcc727390deaa516d8611fbe
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-17 09:42:18 +0200

    added statistics container

---
+++ src/AtomicComponents/Organism/Statistics.tsx
@@ -0,0 +1,10 @@
+import { StatisticsContainer } from '../Atoms/StyledContainers';
+import { DisplayContainerSign } from '../Atoms/DisplayContainerSign';
+
+export const Statistics = () => {
+  return (
+    <StatisticsContainer>
+      <DisplayContainerSign>STATISTICS</DisplayContainerSign>
+    </StatisticsContainer>
+  );
+};
