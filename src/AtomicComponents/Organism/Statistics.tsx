38914a3eddebd132741f3925fd521b169f944565
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-17 23:08:11 +0200

    added User work info component

--- src/AtomicComponents/Organism/Statistics.tsx
+++ src/AtomicComponents/Organism/Statistics.tsx
@@ -1,10 +1,12 @@
 import { StatisticsContainer } from '../Atoms/StyledContainers';
 import { DisplayContainerSign } from '../Atoms/DisplayContainerSign';
+import { UserWorkInfo } from './UserWorkInfo';
 
 export const Statistics = () => {
   return (
     <StatisticsContainer>
       <DisplayContainerSign>STATISTICS</DisplayContainerSign>
+      <UserWorkInfo />
     </StatisticsContainer>
   );
 };
