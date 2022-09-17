92c64f5b0e561b4962eb7605f310f740bb628674
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-17 23:07:20 +0200

    added styles

--- src/AtomicComponents/Atoms/DisplayContainerSign.tsx
+++ src/AtomicComponents/Atoms/DisplayContainerSign.tsx
@@ -7,8 +7,7 @@ export const DisplayContainerSign = styled.h3`
   padding: 0;
   color: #05396e;
   font-family: Roboto;
-  grid-column-start: 2;
-  grid-column-end: 9;
+  grid-column: 2 / 20;
   grid-row-start: 1;
   grid-row-end: 4;
   font-size: 3.5vw;
