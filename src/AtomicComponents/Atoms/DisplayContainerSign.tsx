670a6482ad10a65739ceb17ee037dccd0ba2daf8
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-09-17 09:41:57 +0200

    added sign for containers

---
+++ src/AtomicComponents/Atoms/DisplayContainerSign.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const DisplayContainerSign = styled.h3`
+  justify-self: stretch;
+  align-self: center;
+  margin: 0;
+  padding: 0;
+  color: #05396e;
+  font-family: Roboto;
+  grid-column-start: 2;
+  grid-column-end: 9;
+  grid-row-start: 1;
+  grid-row-end: 4;
+  font-size: 3.5vw;
+`;
