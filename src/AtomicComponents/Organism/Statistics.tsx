2d02adc81bb50045f29603d5d8f10b6c932d9a2a
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-22 19:01:45 +0200

    added props

--- src/AtomicComponents/Organism/Statistics.tsx
+++ src/AtomicComponents/Organism/Statistics.tsx
@@ -1,14 +1,27 @@
+import React from 'react';
 import { StatisticsContainer } from '../Atoms/StyledContainers';
 import { DisplayContainerSign } from '../Atoms/DisplayContainerSign';
 import { UserWorkInfo } from './UserWorkInfo';
 import { DoneTaskPieChart } from './DoneTaskPieChart';
 import { DoneAreaPieChart } from './DoneAreaPieChart';
 
-export const Statistics = () => {
+export interface Props {
+  msFromStart: number;
+  setMsFromStart: React.Dispatch<React.SetStateAction<number>>;
+  timerOn: boolean;
+}
+
+export const Statistics = (props: Props) => {
+  const { msFromStart, setMsFromStart, timerOn } = props;
+
   return (
     <StatisticsContainer>
       <DisplayContainerSign>STATISTICS</DisplayContainerSign>
-      <UserWorkInfo />
+      <UserWorkInfo
+        msFromStart={msFromStart}
+        setMsFromStart={setMsFromStart}
+        timerOn={timerOn}
+      />
       <DoneTaskPieChart />
       <DoneAreaPieChart />
     </StatisticsContainer>
