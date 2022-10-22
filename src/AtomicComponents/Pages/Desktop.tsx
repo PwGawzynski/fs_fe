37e99294669f642a604b30ea26b8dd9b36f00ad3
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-22 19:00:31 +0200

    added timer state and valueOfTimer state to be in desktop because of it connect statistic and control center

--- src/AtomicComponents/Pages/Desktop.tsx
+++ src/AtomicComponents/Pages/Desktop.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { TopBar } from '../Organism/TopBar';
 import { useAuth } from '../../Utils/Hooks/authHook';
 import {
@@ -11,14 +12,24 @@ import { Control } from '../Organism/Control';
 export const Desktop = () => {
   const { user } = useAuth();
   const { worker, owner } = user;
+  const [msFromStart, setMsFromStart] = useState(0);
+  const [timerOn, setTimerOnOff] = useState(true);
+
   return (
     <DesktopMainContainer>
       <TopBar />
       <DesktopBg />
       {worker && owner}
       <OperationCenter>
-        <Statistics />
-        <Control />
+        <Statistics
+          msFromStart={msFromStart}
+          setMsFromStart={setMsFromStart}
+          timerOn={timerOn}
+        />
+        <Control
+          setTimerOnOff={setTimerOnOff}
+          setMsFromStart={setMsFromStart}
+        />
       </OperationCenter>
     </DesktopMainContainer>
   );
