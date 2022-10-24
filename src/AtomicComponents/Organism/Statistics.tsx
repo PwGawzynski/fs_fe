d79c5da6b48e2a550209276b27babb1aab225906
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-10-24 20:14:15 +0200

    added properties

--- src/AtomicComponents/Organism/Statistics.tsx
+++ src/AtomicComponents/Organism/Statistics.tsx
@@ -7,22 +7,36 @@ import { DoneAreaPieChart } from './DoneAreaPieChart';
 
 export interface Props {
   msFromStart: number;
+  msNapFromStart: number;
   setMsFromStart: React.Dispatch<React.SetStateAction<number>>;
+  setMsNapFromStart: React.Dispatch<React.SetStateAction<number>>;
   setTimerOnOff: React.Dispatch<React.SetStateAction<boolean>>;
   timerOn: boolean;
+  napTimerOff: boolean;
 }
 
 export const Statistics = (props: Props) => {
-  const { msFromStart, setMsFromStart, timerOn, setTimerOnOff } = props;
+  const {
+    msFromStart,
+    setMsFromStart,
+    timerOn,
+    setTimerOnOff,
+    setMsNapFromStart,
+    msNapFromStart,
+    napTimerOff,
+  } = props;
 
   return (
     <StatisticsContainer>
       <DisplayContainerSign>STATISTICS</DisplayContainerSign>
       <UserWorkInfo
+        msNapFromStart={msNapFromStart}
         msFromStart={msFromStart}
         setMsFromStart={setMsFromStart}
         timerOn={timerOn}
         setTimerOnOff={setTimerOnOff}
+        setMsNapFromStart={setMsNapFromStart}
+        napTimerOff={napTimerOff}
       />
       <DoneTaskPieChart />
       <DoneAreaPieChart />
