38287daed60c336723509487a2911162537436c0
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-11-08 22:05:23 +0100

    work in progress

--- src/AtomicComponents/Atoms/InsideCenterText.tsx
+++ src/AtomicComponents/Atoms/InsideCenterText.tsx
@@ -4,10 +4,13 @@ import { DisplayContainerSign } from './DisplayContainerSign';
 export interface Props {
   gridRowStart: number;
   color?: string;
+  gridColStart?: number;
 }
 
 export const InsideCenterText = styled(DisplayContainerSign)`
   grid-row-start: ${(props: Props) => `${props.gridRowStart}`};
+  grid-column-start: ${(props: Props) => `${props.gridColStart || 0}`};
   grid-row-end: ${(props: Props) => `${props.gridRowStart + 3}`};
   color: ${(props: Props) => props.color || '#05396e'};
+  font-size: 100%;
 `;
