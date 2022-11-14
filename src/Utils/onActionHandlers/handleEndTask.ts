c58d97634e3a097f208003d7bd46d6f114d42f9f
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-11-14 08:52:19 +0100

    added fn to handle task end

---
+++ src/Utils/onActionHandlers/handleEndTask.ts
@@ -0,0 +1,38 @@
+import { NavigateFunction } from 'react-router-dom';
+import { SerializedTaskResponse } from 'types';
+import React from 'react';
+import { NotificationContextObj } from '../../ContextFactories/NotificationsContext';
+import { Api } from '../Api/Api';
+import { DesktopSettingsContextI } from '../../ContextFactories/DesktopSettingsContext';
+
+export const handleEndTask = async (
+  data: SerializedTaskResponse,
+  nav: NavigateFunction,
+  notification: NotificationContextObj,
+  settings: DesktopSettingsContextI,
+  setCurrentTask: React.Dispatch<
+    React.SetStateAction<SerializedTaskResponse | undefined>
+  >,
+) => {
+  const res = await Api.endTask(data.id);
+  if (!res.status) {
+    notification.setNotification({
+      display: true,
+      message:
+        'Sorry we cannot end this task, something went wrong, please try again later',
+    });
+    nav('../desktop');
+    return;
+  }
+  notification.setNotification({
+    display: true,
+    message: 'Task correctly ended !',
+  });
+  settings.setDesktopSettings((prev) => ({
+    ...prev,
+    OperationCenterHeight: '65vh',
+    bgPhotoShowed: true,
+  }));
+  setCurrentTask(undefined);
+  nav('../desktop');
+};
