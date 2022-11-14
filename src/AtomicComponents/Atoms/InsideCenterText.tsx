d499e8ca5778c27effbe4dbc6be73914260c8331
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-11-14 08:52:57 +0100

    added text align style as props

--- src/AtomicComponents/Atoms/InsideCenterText.tsx
+++ src/AtomicComponents/Atoms/InsideCenterText.tsx
@@ -5,6 +5,7 @@ export interface Props {
   gridRowStart: number;
   color?: string;
   gridColStart?: number;
+  textAlign?: string;
 }
 
 export const InsideCenterText = styled(DisplayContainerSign)`
@@ -12,5 +13,6 @@ export const InsideCenterText = styled(DisplayContainerSign)`
   grid-column-start: ${(props: Props) => `${props.gridColStart || 0}`};
   grid-row-end: ${(props: Props) => `${props.gridRowStart + 3}`};
   color: ${(props: Props) => props.color || '#05396e'};
+  text-align: ${(props: Props) => props.textAlign || 'start'};
   font-size: 100%;
 `;
