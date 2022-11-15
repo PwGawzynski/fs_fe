945fe6dd425fba6e81eceddea4b1037eb1209375
Paweł Gawżyński <80092350+PwGawzynski@users.noreply.github.com>
2022-11-15 10:06:12 +0100

    added possibility to define fn with be called when accept clicked

--- src/ContextFactories/NotificationsContext.tsx
+++ src/ContextFactories/NotificationsContext.tsx
@@ -3,6 +3,7 @@ import React, { createContext } from 'react';
 export interface NotificationsContextI {
   display: boolean;
   message: string;
+  onClickAction?: () => void;
 }
 export interface NotificationContextObj {
   notification: NotificationsContextI;
